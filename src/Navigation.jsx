017a8377aaa737159086dc6fed733de09d0336a8
Nevena Raovic <nevena.raovic@leoilab.com>
2020-10-19 21:34:19 +0200

    Create react app with GTM setup

---
+++ src/Navigation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+function Navigation(props) {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default withRouter(Navigation)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+import TagManager from 'react-gtm-module'
+
+const env = process.env.NODE_ENV
+
+const tagManagerArgs = {
+  gtmId: '<your GTM ID>',
+  dataLayer: {
+    userId: '001',
+    userProject: 'project',
+  },
+  //the below changes GTM values based on whether dev or production
+  auth: env === 'development' ? '<dev ga_auth>' : '<live ga_auth>',
+  preview: env === 'development' ? '<dev ga_env>' : '<live ga_env>',
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
